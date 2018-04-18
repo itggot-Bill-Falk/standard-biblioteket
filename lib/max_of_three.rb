4f947af360d74156175b9b6d5cea7b4a5b8772c8
itggot-Bill-Falk <bill.falk@itggot.se>
2018-04-18 14:46:15 +0200

    max of three redone

--- lib/max_of_three.rb
+++ lib/max_of_three.rb
@@ -1,14 +1,14 @@
-def largest_of_three(number1,number2,number3)
+def max_of_three(number1,number2,number3)
     if number1 < number2 && number3 < number2
-        largest = number2
+        output = number2
     end
 
     if number2 < number1 && number3 < number1
-        largest = number1
+        output = number1
     elsif 
         number2 < number3 && number1 < number3
-            largest = number3
+            output = number3
     end
-    return largest
+    return output
 end
 
