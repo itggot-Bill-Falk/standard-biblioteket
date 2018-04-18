fd130c4db330eaca49119ccff1015d1401e9c3d5
itggot-Bill-Falk <bill.falk@itggot.se>
2018-04-18 13:45:33 +0200

    Is_even

--- dev/is_even.rb
+++
@@ -1,4 +0,0 @@
-def is_even(number)
-    output = true
-    if number/2 == number_to.f
-        
---
+++ lib/is_even.rb
@@ -0,0 +1,9 @@
+def is_even(number)
+    output = number%2
+        if output == 0
+             output = true
+        else
+            output = false
+        end
+    return output
+end
