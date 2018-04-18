fd240ee4f2d0a4b288cffb8708e79841fcec5ab3
itggot-Bill-Falk <bill.falk@itggot.se>
2018-04-18 14:02:32 +0200

    absolute

---
+++ lib/absolute.rb
@@ -0,0 +1,11 @@
+def absolute(number)
+    output = number
+        if number < 0
+            number = number*-1
+            output = number
+        else
+            output = number
+        end 
+    return output
+end
+
