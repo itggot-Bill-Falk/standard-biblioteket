446ffd225d30d4e007e20a0e509a9cde41c673a8
itggot-Bill-Falk <bill.falk@itggot.se>
2018-05-02 13:43:55 +0200

    first of

---
+++ lib/First_of.rb
@@ -0,0 +1,13 @@
+# arr - the array to have its first number extracted
+#
+# Examples
+#   first_of([1, 2, 3]) #=> 1
+#   first_of([1337, 2, -1]) #=> 1337
+#   first_of([0, 0, 0]) #=> 0
+#
+# Return the first number
+def first_of(arr)
+    output = arr[0]
+    return output
+end
+
