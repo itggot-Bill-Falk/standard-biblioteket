a48db4cc7cafdbcd0028ecb6ed236ae6ccdf0546
itggot-Bill-Falk <bill.falk@itggot.se>
2018-05-02 13:15:10 +0200

    Factorial

---
+++ dev/factorial.rb
@@ -0,0 +1,18 @@
+# num - the integer whos previous digits are to be multiplied with each other
+#
+# Examples
+#   factorial(3) #=> 6
+#   factorial(5) #=> 120
+#   factorial(10) #=> 3628800
+#
+# Return the product
+def factorial(num)
+    i = 0
+    product = 1
+        while i < num
+            i += 1
+            sum *= i
+        end
+    return product
+end
+
