e9ef7092694e249d8a00d94f17c2ee3ac7aaebb5
itggot-Bill-Falk <bill.falk@itggot.se>
2018-05-02 13:57:22 +0200

    append

---
+++ lib/append.rb
@@ -0,0 +1,13 @@
+# arr - the array to be expanded
+# num - the integer to be added to the array
+#
+# Examples
+#   append([1, 2, 3], 4) #=> [1, 2, 3, 4]
+#
+# Return the new array
+def append(arr,num)
+   output = arr << num
+    return output
+end
+
+
